9f02255637c20a7a12fc8cf77664075b73b5f78e
Ubuntu <ubuntu@ip-172-31-7-12.us-west-1.compute.internal>
2018-02-03 22:23:01 +0000

    misc

--- config/setup.sh
+++ config/setup.sh
@@ -7,32 +7,32 @@ sudo apt-get update
 
 # Nginx
 sudo apt-get -y install nginx
-sudo mkdir -p /var/www/benrowles.com
-# sudo cp nginx/benrowles.com /etc/nginx/sites-available
-# if [ ! -f /etc/nginx/sites-enabled/benrowles.com ]
-# then
-#     sudo ln -s /etc/nginx/sites-available/benrowles.com /etc/nginx/sites-enabled/benrowles.com
-# fi
-
-if [ ! -f /var/www/benrowles.com/public ]
+sudo mkdir -p /var/www/benrowles.com/logs
+sudo cp nginx/benrowles.com /etc/nginx/sites-available
+if [ ! -L /etc/nginx/sites-enabled/benrowles.com ]
 then
-   sudo ln -s /home/ubuntu/src/website/public /var/www/benrowles.com/public
+    sudo ln -s /etc/nginx/sites-available/benrowles.com /etc/nginx/sites-enabled/benrowles.com
 fi
 
-
+if [ ! -L /var/www/benrowles.com/public ]
+then
+    sudo ln -sT /home/ubuntu/src/website/public /var/www/benrowles.com/public
+fi
 
 # Let's Encrypt
-sudo apt-get install -y python-certbot-nginx
-if [ ! -f /usr/bin/cerbot-auto ]
+if [ ! -f /usr/bin/certbot-auto ]
 then
     sudo wget https://dl.eff.org/certbot-auto
     sudo chmod a+x certbot-auto
-    sudo mv cerbot-auto /usr/bin
+    sudo mv certbot-auto /usr/bin
 fi
 
-sudo certbot-auto --nginx -d benrowles.com -d www.benrowles.com
-sudo cp systemd/* /etc/systemd/system
-sudo systemctl enable cerbot.timer
+if [ ! -f /etc/systemd/system/certbot.service ]
+then
+    sudo certbot-auto --nginx -d benrowles.com -d www.benrowles.com
+    sudo cp systemd/* /etc/systemd/system
+    # sudo systemctl enable certbot.timer
+fi
 
 # Restart nginx
 sudo service nginx restart
